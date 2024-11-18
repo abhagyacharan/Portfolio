d670c1a2d2e324e96e290d1e93f9fa99b15f82c9
Bhagya Charan Aela <a.bhagyacharan@gmail.com>
2024-11-19 03:16:34 +0530

    Final Portfolio

---
+++ ABC_Portfolio/config.js
@@ -0,0 +1,120 @@
+window.abc = {
+  name: 'A Bhagya Charan',
+  jobTitle: 'Data Science Undergrad',
+  jobDescription: 'A passionate tech enthusiast focused on building practical solutions and exploring new technologies. I thrive on turningideas into real-world applications while constantly learning and growing. My drive to create meaningful projects is matched by my dedication to staying current with emerging tech trends.',
+  skills: [
+    {
+      name: 'Programming Languages',
+      stacks:'Python, Java'
+    },
+    {
+      name: 'Data Science',
+      stacks:'Numpy, Pandas, Matplotlib, Scikit-learn, etc'
+    },
+    {
+      name: 'Machine Learning',
+      stacks:'Classification, Regression, Clustering, Dimensionality Reduction'
+    },
+    {
+      name: 'Deep Learning',
+      stacks:'Tensorflow, Pytorch, CNNs, RNNs, GANs, Vision Transformers, Transfer Learning'
+    },
+    {
+      name: 'Web Development',
+      stacks:'React, HTML, CSS, JS, Bootstrap'
+    },
+    
+    {
+      name: 'Natural Language Processing',
+      stacks: 'NLTK, spaCy, LSTM, Hugging Face Transformers'
+    },
+    {
+      name: 'Frameworks',
+      stacks:'Django, Flask, Lang-Chain, Groq-API Integration'
+    },
+    {
+      name: 'Databases',
+      stacks:'MySQL, PostgreSQL'
+    },
+    
+   
+  ],
+  aboutme:[
+    {
+      description: 'I am A Bhagya Charan, a Computer Science undergraduate from Hyderabad, India. A Self-driven, passionate tech enthusiast focused on building practical solutions and exploring new technologies. I thrive on turning ideas into real-world applications while constantly learning and growing. My drive to create meaningful projects is matched by my dedication to staying current with emerging tech trends. I enjoy problem-solving and coding. Always strive to bring 100% to the work I do. Waiting for AGI to take the world by storm.',
+      interests1: '- Learn new Tech / Frameworks',
+      interests2: '- Develop DL Models & Apps',
+      interests3: '- F1 Enthusiast',
+      interests4: '- PC Geek',
+      interests5: '- Anime',
+      fp1: '- Work in MAANG',
+      fp2: '- Build my own SaaS',
+      fp3: '- Get Ready for AGI',
+      fp4: '- Build a Finance Portfolio ',
+      fp5: '- Travel the World',
+    },
+   
+  ],
+
+  projects:[
+    {
+      name: 'CalorEase',
+      link: 'https://github.com/abhagyacharan/CalorEase',
+      hostedLink: 'https://github.com/abhagyacharan/CalorEase',
+      description: 'This Django-based app helps users identify food items, provides calorie and macronutrient details, and estimates burn times for various activities, using image classification and large language model integration.',
+      stack: 'Python | Lang-Chain | Django | Tensorflow | HTML | CSS | JS'
+    },
+    {
+      name: 'Diagno-Guide',
+      link: 'https://github.com/abhagyacharan/Diagno-guide',
+      hostedLink: 'https://github.com/abhagyacharan/Diagno-guide',
+      description: 'A Machine Learning-based system for disease prediction and personalized medicine recommendations. This comprehensive platform includes disease descriptions, precautionary measures, and integrated geolocation services to identify nearby hospitals for enhanced accessibility and healthcare support.',
+      stack: 'Python | Flask | ML | HTML | CSS | JS | AJAX'
+    },
+    {
+      name: 'Vision Transformer',
+      link: 'https://github.com/abhagyacharan/Vision-Transformer-Implementation-',
+      hostedLink:'https://github.com/abhagyacharan/Vision-Transformer-Implementation-',
+      description: 'This project implements a Vision Transformer (ViT) from scratch, inspired by the original ViT paper, to classify between daisies and dandelions. Unlike traditional Convolutional Neural Networks (CNNs), which rely on localized receptive fields, the Vision Transformer leverages self-attention mechanisms, allowing it to capture long-range dependencies in the image.',
+      stack: ' Python | PyTorch | ViT | Numpy | Matplotlib'
+    },
+    {
+      name: 'Note-Taking App using Djano-React',
+      link: 'https://github.com/abhagyacharan/Django-React',
+      hostedLink:'https://github.com/abhagyacharan/Django-React',
+      description: 'A robust note-taking app built with React for the frontend and Django for the backend. It features secure user authentication (registration and login) and allows users to create and delete notes. All data is efficiently managed using a PostgreSQL database.',
+      stack: 'Python | Django | React | PostgreSQL | HTML | CSS'
+    },
+    {
+      name: 'Image Upscaling using Super-Resolution GANs',
+      link: 'https://github.com/abhagyacharan/Image-Upscaling-using-SRGAN',
+      hostedLink:'https://github.com/abhagyacharan/Image-Upscaling-using-SRGAN',
+      description: 'Developed an advanced image upscaling application using SRGAN (Super-Resolution Generative Adversarial Network) capable of transforming low-resolution images into high-resolution versions with enhanced detail and quality. The application leverages deep learning to deliver sharp, realistic image outputs suitable for various use cases.',
+      stack: 'Python | Tensorflow | OpenCV | PIL | Numpy'
+    },
+    {
+      name: 'Colorizer ',
+      link: 'https://github.com/abhagyacharan/Colorizing-Black-and-White-Images',
+      hostedLink: 'https://github.com/abhagyacharan/Colorizing-Black-and-White-Images',
+      description: 'This project uses a Caffe pre-trained model [colorization_deploy_v2.prototxt and colorization_release_v2.caffemodel] to colorize black and white images, using OpenCV. ',
+      stack: 'Python | OpenCV | Numpy | Caffe '
+    }
+  ],
+ 
+
+  links: [
+    {
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>',
+  link: 'https://github.com/abhagyacharan',
+    },
+    {
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/></svg>',
+      link: 'https://www.linkedin.com/in/abhagyacharan/'
+    },
+    {
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16"><path d="M5.026 15c6.037 0 9.354-5.01 9.354-9.35 0-.142 0-.284-.01-.425a6.71 6.71 0 0 0 1.654-1.708 6.58 6.58 0 0 1-1.887.517A3.278 3.278 0 0 0 15.457 3a6.559 6.559 0 0 1-2.085.798A3.278 3.278 0 0 0 7.87 5.09a9.325 9.325 0 0 1-6.77-3.43A3.278 3.278 0 0 0 2.47 7.9a3.251 3.251 0 0 1-.698-.19v.02a3.278 3.278 0 0 0 2.632 3.208 3.221 3.221 0 0 1-.87.116c-.213 0-.422-.02-.627-.06a3.277 3.277 0 0 0 3.061 2.26A6.585 6.585 0 0 1 1 13.555a9.324 9.324 0 0 0 5.026 1.47" /></svg>',
+      link: 'https://x.com/abhagyacharan'
+    }
+  ]
+  
+};
